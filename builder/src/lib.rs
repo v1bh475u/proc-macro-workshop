62df4d45be3753d55667d0b41ac942a81bad5093
vibhatsu <maulikbarot2915@gmail.com>
2026-02-10 23:46:06 +0530

    feat(builder): pass test-06
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,13 @@
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse_macro_input, Data, Fields};
 use quote::quote;
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, GenericArgument, PathArguments, PathSegment,
+    Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    println!("INPUT: {:#?}", input);
+    // eprintln!("INPUT: {:#?}", input);
     let input = parse_macro_input!(input as DeriveInput);
     let tokens = generate_builder(&input);
     eprintln!("TOKENS:\n{}", tokens);
@@ -14,23 +17,30 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 fn generate_builder(input: &DeriveInput) -> TokenStream {
     let name = &input.ident;
     let builder_name = syn::Ident::new(&format!("{}Builder", name), name.span());
-    
+
     let data = match &input.data {
         Data::Struct(data) => data,
-        _ => panic!("Builder can only be derived for structs"),
+        _ => unimplemented!("Builder can only be derived for structs"),
     };
 
     let fields = match &data.fields {
         Fields::Named(fields) => &fields.named,
-        _ => panic!("Builder can only be derived for structs with named fields"),
+        _ => unimplemented!("Builder can only be derived for structs with named fields"),
     };
 
     let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
-        let ty = f.ty.clone();
-        quote! {
-            #name: Option<#ty>
-        }          
+        let ty = &f.ty;
+
+        if is_option_type(ty) {
+            quote! {
+                #name : #ty
+            }
+        } else {
+            quote! {
+                #name: Option<#ty>
+            }
+        }
     });
 
     let builder_struct = quote! {
@@ -39,17 +49,25 @@ fn generate_builder(input: &DeriveInput) -> TokenStream {
         }
     };
 
-    let field_names = fields.iter().map(|f| {
-        &f.ident
-    });
-    
+    let field_names = fields.iter().map(|f| &f.ident);
+
     let builder_pattern = fields.iter().map(|f| {
         let name = &f.ident;
-        let ty = f.ty.clone();
-        quote! {
-            fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        let ty = &f.ty;
+
+        if let Some(inner_ty) = extract_option_inner_type(ty) {
+            quote! {
+                fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
@@ -57,15 +75,23 @@ fn generate_builder(input: &DeriveInput) -> TokenStream {
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let name_str = name.as_ref().unwrap().to_string();
-        quote! {
-            #name : self.#name.clone().ok_or_else(|| format!("field {} is required",#name_str ))?
+
+        if is_option_type(&f.ty)
+        {
+            quote! {
+                #name : self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name : self.#name.clone().ok_or_else(|| format!("field {} is required",#name_str ))?
+            }
         }
     });
 
-    let expanded =quote! {
+    let expanded = quote! {
         use std::error::Error;
         #builder_struct
-        
+
         impl #name {
             pub fn builder() -> #builder_name {
                 #builder_name {
@@ -84,4 +110,23 @@ fn generate_builder(input: &DeriveInput) -> TokenStream {
         }
     };
     proc_macro::TokenStream::from(expanded)
-}    
+}
+
+fn is_option_type(ty: &Type) -> bool {
+    extract_option_inner_type(ty).is_some()
+}
+
+fn extract_option_inner_type(ty: &Type) -> Option<&Type> {
+    if let Type::Path(TypePath { qself: None, path }) = ty {
+        if let Some(PathSegment { ident, arguments }) = path.segments.last() {
+            if ident == "Option" {
+                if let PathArguments::AngleBracketed(args) = arguments {
+                    if let Some(GenericArgument::Type(inner_ty)) = args.args.first() {
+                        return Some(inner_ty);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
